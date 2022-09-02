90c628e2fa8b51a47a8dfad55b36c4244a324e5f
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-09-02 19:56:40 +0100

    updated

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,11 +3,11 @@ import styles from '../../styles/header.module.scss';
 function Header() {
     return (
         <header
-            className={`${styles.container} w-full fixed pr-10 pl-10 items-center bg-black`}
+            className={`${styles.container} w-full fixed pr-10 pl-10 items-center bg-white`}
         >
             <div className="flex flex-row items-center h-full items-center">
                 <img
-                    src="/images/logo.png"
+                    src="/images/logo-black.png"
                     alt="logo"
                     className={styles.logo}
                 />
